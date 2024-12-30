964a873a0deb7c8aef5ea01f5f7f22698e7bcfd4
Alexander Markov <markov_av_77@mail.ru>
2024-12-30 18:22:44 +0400

    4_1 works but long

--- 4_1_phone_guide.cpp
+++ 4_1_phone_guide.cpp
@@ -5,26 +5,55 @@ using namespace std;
 int main()
 {
     cout << "\033[2J\033[1;1H";
-    cout << "Input your request" << endl;
-    string input;
-    string a_name, ph_num;
-    getline(cin, input);
-    bool add_request = false;
-    for (char ch : input)
-    {
-        if (ch == ' ')
-        {
+    cout << "Welcome to our phone guide!!!" << endl;
+    map<string, string> ph_guide_map;
+
+    cout << "Here you can input your request - add or get name of abonent or phone number" << endl;
+    cout << "If you want to finish work - input finish" << endl;
+    bool work_flag = true;
+    do {
+        cout << "Input your request " << endl;
+        string input;
+        string a_name, ph_num;
+        getline(cin, input);
+        bool add_request = false;
+        bool name_exist = false;
+        bool phn_exist = false;
+        for (char ch : input) {
+            if (ch == ' ') {
             add_request = true;
             continue;
+            }
+            if ((ch >= '0' && ch <= '9') || ch == '-') {
+                ph_num += ch;
+                phn_exist = true;
+            }
+            if ((ch >= 'A' && ch <= 'z')) {
+                a_name += ch;
+                name_exist = true;
+            }
         }
-        if ((ch >= '0' && ch <= '9') || ch <= '-')
-        {
-            ph_num += ch;
+        if (a_name == "finish") {
+            work_flag = false;
+            cout << "Working of programme has finished!" << endl; 
         }
-        if ((ch >= 'A' && ch <= 'z'))
-        {
-            a_name += ch;
+        else if (add_request && phn_exist && name_exist) {
+            ph_guide_map[ph_num] = a_name;
         }
-    }
-    cout << "Abonent: " << a_name << " " << ph_num;
+        else if (phn_exist && !add_request) {
+            if (ph_guide_map.find(ph_num) != ph_guide_map.end()) {
+                cout << "Abonent of this phone number is " << ph_guide_map[ph_num] << endl;
+            }
+            else {
+                cout << "Abonent of this phone number not found!";
+            }
+        }
+        else if (name_exist && !add_request) {
+            for (const auto &line : ph_guide_map) {
+                if (line.second == a_name) {
+                    cout << line.second << " has phone number " << line.first << endl;
+                }
+            }
+        }
+    } while (work_flag);
 }
